0c08f8d2353d58cbbeba74a441f98e9619e48308
nitin-singla <nitingithub@gmail.com>
2023-06-07 21:16:57 +0530

    gradient_container.dart added - code splitting

---
+++ lib/gradient_container.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  // const GradientContainer({key}) : super(key: key); // works as well
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.redAccent,
+            Colors.orangeAccent,
+            Colors.yellow,
+            Color.fromARGB(255, 157, 221, 214),
+            Colors.white,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Flutter! ~ Nitin',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.purple,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(body: GradientContainer()),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  // const GradientContainer({key}) : super(key: key); // works as well
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.redAccent,
-            Colors.orangeAccent,
-            Colors.yellow,
-            Color.fromARGB(255, 157, 221, 214),
-            Colors.white,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello Flutter! ~ Nitin',
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.purple,
-          ),
-        ),
-      ),
-    );
-  }
 }
