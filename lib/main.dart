98b0527dde2b87d5d8c23b992ada9e0d967c3257
nitin-singla <nitingithub@gmail.com>
2023-06-07 21:12:02 +0530

    Extracted Container Widget into a separate class

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.redAccent,
-                Colors.orangeAccent,
-                Colors.yellow,
-                Color.fromARGB(255, 157, 221, 214),
-                Colors.white,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello Flutter! ~ Nitin',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.purple,
-              ),
-            ),
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  // const GradientContainer({key}) : super(key: key); // works as well
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.redAccent,
+            Colors.orangeAccent,
+            Colors.yellow,
+            Color.fromARGB(255, 157, 221, 214),
+            Colors.white,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Flutter! ~ Nitin',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.purple,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
