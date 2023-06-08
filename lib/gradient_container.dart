0d61fd01794e78f781a0ff2178f46383d55a4313
nitin-singla <nitingithub@gmail.com>
2023-06-08 20:55:28 +0530

    Introducing variables in the code.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/styled_text.dart';
 
+var beginAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   // const GradientContainer({key}) : super(key: key); // works as well
@@ -9,17 +12,19 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
+        // using const BoxDecoration here with var beginAlignment & var endAlignment results in the error - "A value of type 'Null' can't be assigned to a parameter of type 'AlignmentGeometry' in a const constructor.
+
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Colors.redAccent,
             Colors.orangeAccent,
             Colors.yellow,
             Color.fromARGB(255, 157, 221, 214),
             Colors.white,
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: beginAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
