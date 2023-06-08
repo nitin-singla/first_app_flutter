16efcd810b04f3589c11b0c6393b3d67e4247357
nitin-singla <nitingithub@gmail.com>
2023-06-09 00:02:37 +0530

    GradientContainer() made a configurable widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,21 +6,17 @@ const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
   // const GradientContainer({key}) : super(key: key); // works as well
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.redAccent,
-            Colors.orangeAccent,
-            Colors.yellow,
-            Color.fromARGB(255, 157, 221, 214),
-            Colors.white,
-          ],
+          colors: colors,
           begin: beginAlignment,
           end: endAlignment,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/gradient_container.dart';
 
+const gradientColors = [
+  Colors.redAccent,
+  Colors.orangeAccent,
+  Colors.yellow,
+  Color.fromARGB(255, 157, 221, 214),
+  Colors.white,
+];
+
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+        body: GradientContainer(
+          gradientColors,
+        ),
+      ),
     ),
   );
 }
